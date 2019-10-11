e490ffec7ccc456033ee34de897b2451391b7d3d
megatol <55156941+megatol@users.noreply.github.com>
2019-10-11 23:32:32 +0200

    Update key.php

--- httpdocs/key.php
+++ httpdocs/key.php
@@ -2,13 +2,13 @@
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
-die('{"key":"-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt2Fgu5KaUq302ZRErgaz
-RM4k6wAEugu9hC86bkOUIXBqCPC+ewXDeVTIOixduAwGrCwkDpJi6mGAfZ6rFLC2
-G/vFB0MdtVWPrVyOdd6jVchs4f0Gx95pqdwrKmqITfhQQX9Hxx61196ibXK8IyBj
-Aswx7j081qeK/jwxCP6gVJG5qJc56c6wJbPWq6KgKue4b8R2MOf/5VEWIDFHWuh4
-UZaG0fWw/98Mr++rueg1Co6oO/HuLjaVEuAFnPEengHZWEw7CZMniI0tH6s13ZRV
-JsilAYkIvrYdamDIlWrIKwALYki2QZ9AvW0Ewzdym7eYWQcfUX+QOLsGaSkZcppM
-7wIDAQAB
------END PUBLIC KEY-----"}');
+die("{\"key\":\"-----BEGIN PUBLIC KEY-----\n".
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt2Fgu5KaUq302ZRErgaz\n".
+    "RM4k6wAEugu9hC86bkOUIXBqCPC+ewXDeVTIOixduAwGrCwkDpJi6mGAfZ6rFLC2\n".
+    "G/vFB0MdtVWPrVyOdd6jVchs4f0Gx95pqdwrKmqITfhQQX9Hxx61196ibXK8IyBj\n".
+    "Aswx7j081qeK/jwxCP6gVJG5qJc56c6wJbPWq6KgKue4b8R2MOf/5VEWIDFHWuh4\n".
+    "UZaG0fWw/98Mr++rueg1Co6oO/HuLjaVEuAFnPEengHZWEw7CZMniI0tH6s13ZRV\n".
+    "JsilAYkIvrYdamDIlWrIKwALYki2QZ9AvW0Ewzdym7eYWQcfUX+QOLsGaSkZcppM\n".
+    "7wIDAQAB\n".
+    "-----END PUBLIC KEY-----\"}");
 ?>
