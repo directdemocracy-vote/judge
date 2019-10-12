333516074bc128bacfc403b281d4140996afeff5
Olivier Michel <Olivier.Michel@cyberbotics.com>
2019-10-12 23:11:28 +0200

    key

--- httpdocs/key.php
+++ httpdocs/key.php
@@ -1,13 +1,22 @@
 <?php
+
+function stripped_key($public_key) {
+  $stripped = "";
+  $header = strlen("-----BEGIN PUBLIC KEY-----\n");
+  $footer = strlen("-----END PUBLIC KEY-----");
+  $l = strlen($public_key) - $footer;
+  for($i = $header; $i < $l; $i += 65)
+    $stripped .= substr($public_key, $i, 64);
+  $stripped = substr($stripped, 0, -1 - footer);
+  return $stripped;
+}
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
-die("{\"key\":\"".
-    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt2Fgu5KaUq302ZRErgaz".
-    "RM4k6wAEugu9hC86bkOUIXBqCPC+ewXDeVTIOixduAwGrCwkDpJi6mGAfZ6rFLC2".
-    "G/vFB0MdtVWPrVyOdd6jVchs4f0Gx95pqdwrKmqITfhQQX9Hxx61196ibXK8IyBj".
-    "Aswx7j081qeK/jwxCP6gVJG5qJc56c6wJbPWq6KgKue4b8R2MOf/5VEWIDFHWuh4".
-    "UZaG0fWw/98Mr++rueg1Co6oO/HuLjaVEuAFnPEengHZWEw7CZMniI0tH6s13ZRV".
-    "JsilAYkIvrYdamDIlWrIKwALYki2QZ9AvW0Ewzdym7eYWQcfUX+QOLsGaSkZcppM".
-    "7wIDAQAB\"}");
+$public_key_file = fopen("../id_rsa.pub", "r") or die("{\"error\":\"unable to open public key file\"}");
+$k = fread($public_key_file, filesize("../id_rsa.pub"));
+fclose($public_key_file);
+$key = stripped_key($k):
+die("{\"key\":\"$key\"}");
 ?>
