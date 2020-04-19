bc20ae3a378a1488dad9b4550f39d5c567084e3c
Galine <webmaster@cyberbotics.com>
2020-04-19 12:01:07 +0200

    fixed

--- httpdocs/endorse.php
+++ httpdocs/endorse.php
@@ -10,6 +10,14 @@ function error($message) {
   die("{\"error\":$message}");
 }
 
+function stripped_key($public_key) {
+  $stripped = str_replace("-----BEGIN PUBLIC KEY-----", "", $public_key);
+  $stripped = str_replace("-----END PUBLIC KEY-----", "", $stripped);
+  $stripped = str_replace("\r\n", '', $stripped);
+  $stripped = str_replace("\n", '', $stripped);
+  return $stripped;
+}
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
