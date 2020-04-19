0040174098f039b4b43bf26d470dfeab1cdf53f7
Galine <webmaster@cyberbotics.com>
2020-04-19 13:26:17 +0200

    updated

--- httpdocs/reputation.php
+++ httpdocs/reputation.php
@@ -1,5 +1,7 @@
 <?php
 require_once '../php/database.php';
+$myself = 'https://trustee.directdemocracy.vote';
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
@@ -10,7 +12,7 @@ $mysqli->set_charset('utf8mb4');
 if (!isset($_GET['key']))
   die("\"error\":\"Missing key argument\"}");
 
-$response = file_get_contents("endorse.php");
+$response = file_get_contents("$myself/endorse.php");
 
 $key = $mysqli->escape_string($_GET['key']);
 $query = "SELECT reputation, endorsed FROM entity WHERE `key`='$key'";
