b44cdc7cfa784ee50e1a2a8a8dacdbdc266406c2
megatol <74714212+megatol@users.noreply.github.com>
2023-08-18 13:25:31 +0200

    Update judge.sql

--- database/judge.sql
+++ database/judge.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `entity` (
+CREATE TABLE `participant` (
   `id` int(11) NOT NULL,
   `key` varchar(512) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
   `signature` varchar(512) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
@@ -16,13 +16,13 @@ CREATE TABLE `status` (
   `lastUpdate` bigint(15) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-ALTER TABLE `entity`
+ALTER TABLE `participant`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `key` (`key`);
 
 ALTER TABLE `link`
   ADD PRIMARY KEY (`endorser`,`endorsed`);
 
-ALTER TABLE `entity`
+ALTER TABLE `participant`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 COMMIT;
