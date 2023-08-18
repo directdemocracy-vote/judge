cad1582c58abb663ac69afb1afdf34cea37b8217
megatol <74714212+megatol@users.noreply.github.com>
2023-08-18 14:37:56 +0200

    Update judge.sql

--- database/judge.sql
+++ database/judge.sql
@@ -2,6 +2,7 @@ CREATE TABLE `participant` (
   `id` int(11) NOT NULL,
   `key` varchar(512) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
   `signature` varchar(512) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `home` point NOT NULL,
   `reputation` float NOT NULL,
   `endorsed` tinyint(1) NOT NULL,
   `changed` tinyint(1) NOT NULL
@@ -9,7 +10,10 @@ CREATE TABLE `participant` (
 
 CREATE TABLE `link` (
   `endorser` int(11) NOT NULL,
-  `endorsed` int(11) NOT NULL
+  `endorsed` int(11) NOT NULL,
+  `revoke` tinyint(1) NOT NULL,
+  `distance` float NOT NULL,
+  `date` bigint(15) NOT NULL,
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 CREATE TABLE `status` (
