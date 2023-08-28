ef6bb7803201fef38a036f96914e41f8ad85267a
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-08-28 16:10:16 +0200

    revert

--- httpdocs/test/ajax/save.php
+++ httpdocs/test/ajax/save.php
@@ -6,9 +6,5 @@
   header('Content-Type: application/json');
   $json = file_get_contents('php://input');
   $data = json_decode($json);
-
-  $name = isset($data->name) ? $data->name : "worldfile";
-  $file = fopen('../storage/'.$name.'.json','w');
-  fwrite($file, $json);
-  fclose($file);
+  die(json_encode($data));
 ?>
