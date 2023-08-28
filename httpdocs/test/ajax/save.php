34e47aeb7f39ab83734246819d11abaa09c61fb1
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-08-28 15:49:11 +0200

    test save

--- httpdocs/test/ajax/save.php
+++ httpdocs/test/ajax/save.php
@@ -6,5 +6,9 @@
   header('Content-Type: application/json');
   $json = file_get_contents('php://input');
   $data = json_decode($json);
-  die(json_encode($data));
+
+  $name = isset($data->name) ? $data->name : "worldfile";
+  $file = fopen('../storage/'.$name.'.json','w');
+  fwrite($file, $json);
+  fclose($file);
 ?>
