276ab691f9a8f0a75701eff33db762f66302362b
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-08-29 11:52:46 +0200

    draw

--- httpdocs/test/js/World.js
+++ httpdocs/test/js/World.js
@@ -455,6 +455,8 @@ export default class World {
             this.#idGenerator = citizen.id + 1;
           this.#citizens.set(citizen.id, undefined, citizen.coords, this.#basePointSize);
         }
+
+        this.#draw();
       });
 
     this.#closeWorldsPanel();
