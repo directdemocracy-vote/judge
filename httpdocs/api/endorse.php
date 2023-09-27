d7af3cbe3049b40ea43f1fdb756d78475e9d5dd5
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-09-27 12:18:31 +0200

    restore

--- httpdocs/api/endorse.php
+++ httpdocs/api/endorse.php
@@ -175,7 +175,7 @@ for($i = 0; $i < 15; $i++) {  # supposed to converge in about 13 iterations
     }
     $r0->free();
     $new_reputation = reputation_function(2 / (1 + sqrt($total_reputation / $N)) + $sum);
-    $query = "UPDATE participant SET reputation=0 WHERE id=$id";
+    $query = "UPDATE participant SET reputation=$new_reputation WHERE id=$id";
     $mysqli->query($query) or error($mysqli->error);
     $query = "UPDATE participant SET endorsed=1, changed=1 WHERE id=$id AND endorsed=0 AND reputation>$threshold";
     $mysqli->query($query) or error($mysqli->error);
