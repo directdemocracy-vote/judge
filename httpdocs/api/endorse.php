c5dadacd83c717e0dc74d39dcfde19b414b8d56a
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-09-27 14:38:02 +0200

    fix order

--- httpdocs/api/endorse.php
+++ httpdocs/api/endorse.php
@@ -177,7 +177,7 @@ for($i = 0; $i < 15; $i++) {  # supposed to converge in about 13 iterations
       $sum += $reputation * $distance_factor * $time_factor;
     }
     $r0->free();
-    $new_reputation = 0;//reputation_function(2 / (1 + sqrt($total_reputation / $N)) + $sum);
+    $new_reputation = reputation_function(2 / (1 + sqrt($total_reputation / $N)) + $sum);
     $query = "UPDATE participant SET reputation=$new_reputation WHERE id=$id";
     $mysqli->query($query) or error($mysqli->error);
     $query = "UPDATE participant SET endorsed=1, changed=1 WHERE id=$id AND endorsed=0 AND reputation>$threshold";
