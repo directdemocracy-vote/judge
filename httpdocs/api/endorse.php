c929d061d3fb4428f08489a1de83a4e7d9772ff9
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-09-27 15:02:58 +0200

    test

--- httpdocs/api/endorse.php
+++ httpdocs/api/endorse.php
@@ -21,7 +21,7 @@ function stripped_key($public_key) {
 function distance_function($distance) {
   if ($distance < 1)
     $distance = 1;
-  die($distance);
+  die("test".$distance);
 
   if ($distance < 10)
     return 1 - (1 / (1 + exp((10 - $distance) / 2)));
