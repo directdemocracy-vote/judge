c2e7c05e45215f3f1c7d572759879af348e45479
megatol <74714212+megatol@users.noreply.github.com>
2023-09-27 09:20:10 +0200

    database initialisation

--- httpdocs/api/endorse.php
+++ httpdocs/api/endorse.php
@@ -22,10 +22,6 @@ function stripped_key($public_key) {
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
-$mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
-if ($mysqli->connect_errno)
-  error("Failed to connect to MySQL database: $mysqli->connect_error ($mysqli->connect_errno)");
-$mysqli->set_charset('utf8mb4');
 
 $now = time();
 
--- httpdocs/api/reputation.php
+++ httpdocs/api/reputation.php
@@ -5,10 +5,7 @@ $myself = 'https://judge.directdemocracy.vote';
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
-$mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
-if ($mysqli->connect_errno)
-  die("{\"error\":\"Failed to connect to MySQL database: $mysqli->connect_error ($mysqli->connect_errno)\"}");
-$mysqli->set_charset('utf8mb4');
+
 if (!isset($_GET['key']))
   die("\"error\":\"Missing key argument\"}");
 
