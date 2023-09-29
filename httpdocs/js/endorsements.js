72c2d242edef6a15685076d0a5b761d5091c7cc7
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-09-29 14:51:11 +0200

    clean files

--- httpdocs/js/endorsements.js
+++ httpdocs/js/endorsements.js
@@ -1,21 +1,21 @@
 window.onload = function() {
   const judge = window.location.protocol + '//' + window.location.host;
   const notary = judge.replace('judge', 'notary');
-  let content = document.getElementById('content');
+  const content = document.getElementById('content');
   fetch(`${notary}/api/judge.php`, {method: 'POST',
     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
     body: `judge=${encodeURIComponent(judge)}`})
-    .then((response) => response.json())
-    .then((answer) => {
+    .then(response => response.json())
+    .then(answer => {
       if (answer.error) {
         console.log(answer.error);
         return;
       }
-      let table = document.createElement('table');
-      let thead = document.createElement('thead');
+      const table = document.createElement('table');
+      const thead = document.createElement('thead');
       table.classList.add('table', 'is-striped');
       table.appendChild(thead);
-      let tr = document.createElement('tr');
+      const tr = document.createElement('tr');
       thead.appendChild(tr);
       let th = document.createElement('th');
       tr.appendChild(th);
@@ -27,12 +27,12 @@ window.onload = function() {
       th = document.createElement('th');
       tr.appendChild(th);
       th.innerHTML = 'Date';
-      let tbody = document.createElement('tbody');
+      const tbody = document.createElement('tbody');
       table.appendChild(tbody);
       for (let i = 0; i < answer.endorsements.length; i++) {
-        let tr = document.createElement('tr');
+        const tr = document.createElement('tr');
         tbody.appendChild(tr);
-        let endorsement = answer.endorsements[i];
+        const endorsement = answer.endorsements[i];
         let td = document.createElement('td');
         if (endorsement.latest) {
           td.setAttribute('align', 'center');
@@ -48,7 +48,7 @@ window.onload = function() {
           td.title = 'revoked';
         } else
           td.title = 'endorsed';
-        let a = document.createElement('a');
+        const a = document.createElement('a');
         td.appendChild(a);
         a.href = `${notary}/citizen.html?fingerprint=${CryptoJS.SHA1(endorsement.signature).toString()}`;
         a.innerHTML = endorsement.givenNames + ' ' + endorsement.familyName;
