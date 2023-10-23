cd4131de4ff55b6a81cbd21e7c0c7ab8dc38f79f
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-10-23 14:44:01 +0200

    test

--- httpdocs/api/endorse.php
+++ httpdocs/api/endorse.php
@@ -56,11 +56,11 @@ $last_update = intval($status['lastUpdate']);
 $query = "SELECT * FROM participant";
 $result = $mysqli->query($query) or error($mysqli->error);
 $data = array();
+die("test");
 while($row = $result->fetch_array(MYSQLI_ASSOC)) {
  array_push($data, $row);
 }
 die(json_encode($data));
-die($data);
 // $update_every = 10;
 // if ($last_update + $update_every > $now)
 //   die("Updated in the last $update_every seconds");
