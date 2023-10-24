c01d356e0e20972c3c49e2651eca6fbf4d1847b3
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-10-24 16:21:58 +0200

    test

--- httpdocs/api/endorse.php
+++ httpdocs/api/endorse.php
@@ -66,7 +66,7 @@ $mysqli->query($query) or error($mysqli->error);
 
 $options = array('http' => array('method' => 'GET',
                                  'header' => "Content-Type: application/json\r\nAccept: application/json\r\n"));
-$url = "$notary/api/publications.php?type=endorsement&published_from=$last_update";
+$url = "$notary/api/publications.php?type=endorsement";
 $response = file_get_contents($url, false, stream_context_create($options));
 $endorsements = json_decode($response);
 if (isset($endorsements->error))
