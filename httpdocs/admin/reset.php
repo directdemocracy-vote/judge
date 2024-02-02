8948ea6f3ecc93d5e87ef7ac00267b47ec80dcb5
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-02-02 15:33:35 +0100

    Update reset.php

--- httpdocs/admin/reset.php
+++ httpdocs/admin/reset.php
@@ -1,8 +1,13 @@
 <?php
 
 require '../../../php/database.php';
+
+function error($error) {
+  die("{\"error\":\"$error\"}");
+}
+
 $mysqli->query("DELETE FROM link") or die($mysqli->error);
 $mysqli->query("DELETE FROM participant") or die($mysqli->error);
 $mysqli->query("UPDATE status SET lastUpdate=0, areaCount=1") or die($mysqli->error);
-die("Reset successful");
+die('{"status":"Reset successful"}');
 ?>
