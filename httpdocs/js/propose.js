eb11f8e40d606bf9e0c4d86159e6088d2f439d58
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-12 17:29:25 +0200

    Update propose.js

--- httpdocs/js/propose.js
+++ httpdocs/js/propose.js
@@ -98,7 +98,7 @@ window.onload = function() {
         document.getElementById('deadline-hour').value = deadlineDate.getHours();
         document.getElementById('trust').value = answer.trust;
         document.getElementById('email').value = answer.email;
-        translator.translateElement('submit', 'modify');
+        translator.translateElement(document.getElementById('submit'), 'modify');
       });
   } else {
     if (latitude === -1) {
