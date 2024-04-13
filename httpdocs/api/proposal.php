dd98558ccd54d579b1977a1f344d0ab3a998fb97
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-13 10:23:23 +0200

    Update proposal.php

--- httpdocs/api/proposal.php
+++ httpdocs/api/proposal.php
@@ -16,7 +16,8 @@ if (!isset($_GET['reference']))
   error('Missing reference parameter');
 $reference = $mysqli->escape_string($_GET['reference']);
 $query = "SELECT type, area, title, description, question, answers, secret, UNIX_TIMESTAMP(publication) AS publication, "
-        ."UNIX_TIMESTAMP(deadline) AS deadline, trust, website, email, language FROM proposal WHERE reference=UNHEX('$reference')";
+        ."UNIX_TIMESTAMP(deadline) AS deadline, trust, website, email, language, countryCode, timeZone, timeZoneOffset "
+        ."FROM proposal WHERE reference=UNHEX('$reference')";
 $result = $mysqli->query($query) or error($mysqli->error);
 $proposal = $result->fetch_assoc();
 $result->free();
@@ -25,6 +26,7 @@ if (!$proposal)
 $proposal['publication'] = intval($proposal['publication']);
 $proposal['deadline'] = intval($proposal['deadline']);
 $proposal['trust'] = intval($proposal['trust']);
+$proposal['timeZoneOffset'] = intval($proposal['timeZoneOffset']);
 $proposal['secret'] = $proposal['secret'] === 1;
 die(json_encode($proposal, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
 ?>
