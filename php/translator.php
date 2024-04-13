15406886d4ce0d4614086a230dedea97266546e5
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-13 15:28:39 +0200

    Update translator.php

--- php/translator.php
+++ php/translator.php
@@ -3,7 +3,7 @@ $translator_dictionary = null;
 
 function translator_init($language) {
   global $translator_dictionary;
-  $json = file_get_contents(__DIR__."../httpdocs/i18n/$language.json"); 
+  $json = file_get_contents(__DIR__."/../httpdocs/i18n/$language.json"); 
   $translator_dictionary = json_decode($json, true); 
 }
 
