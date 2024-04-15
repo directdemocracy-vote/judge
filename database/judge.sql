82cb9159c6e2cc84f736a36123df6b1edf5b9d12
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-04-15 17:02:09 +0200

    Update judge.sql

--- database/judge.sql
+++ database/judge.sql
@@ -26,6 +26,26 @@ CREATE TABLE `status` (
   `areaCount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE `proposal` (
+  `id` int(11) NOT NULL,
+  `reference` binary(20) NOT NULL,
+  `type` enum('petition','referendum','election','') NOT NULL,
+  `area` text NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `question` varchar(128) NOT NULL,
+  `answers` text NOT NULL,
+  `secret` tinyint(1) NOT NULL,
+  `website` varchar(2048) NOT NULL,
+  `publication` datetime NOT NULL,
+  `deadline` datetime NOT NULL,
+  `trust` bigint(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `language` varchar(3) NOT NULL COMMENT 'ISO 639 code',
+  `countryCode` varchar(2) NOT NULL,
+  `timeZone` varchar(255) NOT NULL,
+  `timeZoneOffset` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 ALTER TABLE `link`
   ADD PRIMARY KEY (`endorser`,`endorsed`),
@@ -34,12 +54,17 @@ ALTER TABLE `link`
 ALTER TABLE `participant`
   ADD PRIMARY KEY (`id`);
 
-
 ALTER TABLE `participant`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
 
 ALTER TABLE `link`
   ADD CONSTRAINT `endorsed` FOREIGN KEY (`endorsed`) REFERENCES `participant` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `endorser` FOREIGN KEY (`endorser`) REFERENCES `participant` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `proposal`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `proposal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 COMMIT;
