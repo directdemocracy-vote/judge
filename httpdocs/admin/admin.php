e6be892c12e345b632fbe002ef3f491d8137d38e
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-05-20 21:14:21 +0200

    Update admin.php

--- httpdocs/admin/admin.php
+++ httpdocs/admin/admin.php
@@ -8,6 +8,6 @@ function error($error) {
 
 $mysqli->query("DELETE FROM link") or die($mysqli->error);
 $mysqli->query("DELETE FROM participant") or die($mysqli->error);
-$mysqli->query("UPDATE status SET lastUpdate=0, areaCount=1") or die($mysqli->error);
+$mysqli->query("UPDATE status SET lastUpdate=0") or die($mysqli->error);
 die('{"status":"Reset successful"}');
 ?>
