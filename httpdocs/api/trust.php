2feabf740cbe6b3567c73ab94d176363e163ef49
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-07-21 22:38:01 +0200

    Update trust.php

--- httpdocs/api/trust.php
+++ httpdocs/api/trust.php
@@ -149,7 +149,7 @@ if ($certificates)
           $context  = stream_context_create(array('http' => array('header' => 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')));
           $url = "$notary/api/locate.php?osm_ids=" . $endorsed->locality . "," . $endorser->locality;
           $json = @file_get_contents($url, false, $context);
-          echo "$url => $json<br>";
+          # echo "$url => $json<br>";
           $localities = json_decode($json);
           if (!empty($localities)) {
             $query = "INSERT IGNORE INTO locality(osm_id, location, name) "
