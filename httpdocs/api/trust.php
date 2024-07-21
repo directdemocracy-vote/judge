5ca288ce9016b938eec37e1396b0747901b234d2
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-07-21 22:31:34 +0200

    Update trust.php

--- httpdocs/api/trust.php
+++ httpdocs/api/trust.php
@@ -147,7 +147,7 @@ if ($certificates)
         $d = $result->fetch_assoc();
         if (!$d) {
           $context  = stream_context_create(array('http' => array('header' => 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')));
-          $url = "https://$notary/api/locate.php?osm_ids=" . $endorsed->locality . "," . $endorser->locality;
+          $url = "$notary/api/locate.php?osm_ids=" . $endorsed->locality . "," . $endorser->locality;
           $json = @file_get_contents($url, false, $context);
           echo "$url => $json<br>";
           if ($json) {
