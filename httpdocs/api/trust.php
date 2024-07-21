b748bed0d5abda9c38e11f7d45fd31b87f38b807
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-07-21 22:57:50 +0200

    Update trust.php

--- httpdocs/api/trust.php
+++ httpdocs/api/trust.php
@@ -151,8 +151,8 @@ if ($certificates)
           $json = @file_get_contents($url, false, $context);
           echo "json = $json<br><br>";
           $localities = json_decode($json, true);
-          echo json_encode($localities['0']);
-          echo $localities[0]->name . "<br>";
+          echo json_encode($localities[0]);
+          echo "<br>" . $localities[0]->name . "<br>";
           if (!empty($localities)) {
             $query = "INSERT IGNORE INTO locality(osm_id, location, name) "
               ."VALUES(".$localities[0]['osm_id'].", ST_PointFromText('POINT(".$localities[0]['longitude']." ".$localities[0]['latitude'].")'), \"".$localities[0]['name']."\")";
